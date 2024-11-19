42ceddb88036690fd499b2d01f021a9fd9fb6c4f
denielvarava <varavadenik16@gmail.com>
2024-11-19 19:46:48 +0000

    Initial commit

---
+++ app/src/main/java/com/example/qrcode/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.qrcode;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+
+public class MainActivity extends AppCompatActivity {
+    EditText etName, etAddress;
+    Button buttonScan;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        etName = findViewById(R.id.etName);
+        etAddress = findViewById(R.id.etAddress);
+        buttonScan = findViewById(R.id.buttonScan);
+    }
+}
